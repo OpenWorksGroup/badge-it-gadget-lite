8526d64575918539a9868ed0e716aa6b4e42b440
Kerri Lemoie <kerrilemoie@gmail.com>
2012-06-26 17:12:02 -0400

    cleaning up settings file more

--- process-badges/gadget-settings.php
+++ process-badges/gadget-settings.php
@@ -12,7 +12,7 @@ Read more about Open Badges Assertions here: https://github.com/mozilla/openbadg
 
 ***/
 
-/* Issuer API url - this is Open Badge's hosted issuer API */
+/* Issuer API url - this is Open Badge's hosted issuer API.  REQUIRED */
 $open_badges_api = "http://beta.openbadges.org/issuer.js";
 
 /*version - Use "0.5.0" for the beta. REQUIRED. */
@@ -21,7 +21,7 @@ $version = "0.5.0";
 /*issuer url - this is the domain name of the site that will be issuing the badges. It should be the domain where you're installing the OpenBadgifier. REQUIRED.*/
 $issuer_url = "http://yourdomain.com";
 
-/*root path - this is the root path of where your process-badges directory is hosted. You SHOULD password protect this directory with something like .htaccess so that the public can't issue badges on your behalf. CHMOD 775 */
+/*root path - this is the root path of where your process-badges directory is hosted. You SHOULD password protect this directory with something like .htaccess so that the public can't issue badges on your behalf. REQUIRED. CHMOD 775 */
 
 $root_path = $_SERVER['DOCUMENT_ROOT']."badge-it-gadget-lite/process-badges/";
 
@@ -37,15 +37,15 @@ $issuer_org = "";
 
 $issuer_contact = "";
 
-/* JSON file directory - OpenBadgifier generates JSON file for each issued badge (per person). The JSON files need to be in a publicly accessible but not obvious directory. This should start at the document root of your host. Note that example has slashes at the end of the path. Please be sure to include. CHMOD 777 */
+/* JSON file directory - OpenBadgifier generates JSON file for each issued badge (per person). The JSON files need to be in a publicly accessible but not obvious directory. This should start at the document root of your host. Note that example has slashes at the end of the path. Please be sure to include. REQUIRED. CHMOD 777 */
 
 $json_dir = $_SERVER['DOCUMENT_ROOT']."badge-it-gadget-lite/digital-badges/issued/json/";
 
-/* badge images directory - Set the path to the directory where your badge images are stored. They should be stored on the same domain as OpenBadifier since the images should be on the issing site. Don't have badge images yet? You can mae some here (note: they must be PNG) - http://www.onlineiconmaker.com/application/ */
+/* badge images directory - Set the path to the directory where your badge images are stored. They should be stored on the same domain as OpenBadifier since the images should be on the issing site. Don't have badge images yet? You can mae some here (note: they must be PNG) - http://www.onlineiconmaker.com/application/  REQUIRED */
 
 $badge_images_dir = "/badge-it-gadget-lite/digital-badges/images/";
 
-/* badge records file - OpenBadgifier will keep records in a text file of which badges were issued and if they were pushed to the obi. This could easily be extended to use a db later. Nice to have a lightweight solution anyone can use. This file has already been created and is in the dircetory where this settings file is. CHMOD 777*/
+/* badge records file - OpenBadgifier will keep records in a text file of which badges were issued and if they were pushed to the obi. This could easily be extended to use a db later. Nice to have a lightweight solution anyone can use. This file has already been created and is in the dircetory where this settings file is. REQUIRED CHMOD 777*/
 
 $badge_records_file = "badge_records.txt";
 
